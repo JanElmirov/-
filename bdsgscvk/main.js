bde45f65c381626302ab194eeee43ca4471f5b13
JanElmirov <99734651+JanElmirov@users.noreply.github.com>
2022-03-01 18:26:12 +0600

    Add files via upload

---
+++ bdsgscvk/main.js
@@ -0,0 +1,174 @@
+// Цикл for
+//let arr = [1,2, 'hello', true ]
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(`Элемент - ${arr[i]} - индекс элемента ${i}`);
+// }
+
+//Цикл While
+// let i = 0
+// while ( i < arr.length ) {
+// 	console.log(arr[i]); i++
+// }
+
+// DO WHile
+// let i = 0
+// do {
+// 	i++
+// 	console.log('Hello');
+// 	}	while (i < 5)
+
+
+//Цикл For of
+// let arr = [1,2, 'hello', true ]
+// for (let  itemArrey of arr) {
+// 	console.log(itemArrey);
+// }
+
+// const obj = {
+// 	name:'Ogogo',
+// 	age: 2,
+// }
+// for (let  i in obj ) {
+// 	console.log(obj[i]);
+// }
+
+
+// let arr = [1,2,3,4]
+// for (let i = 0; i < arr.length; i++) {
+// 	if ( arr[i] === 1) {
+// 		arr[i] = 0
+// 	}
+// }
+// arr.push('hello') // добавление в конец внутри массива
+// //arr.pop() // Удаление добавленнного через push
+// console.log(arr);
+
+// let allNamePersonal = [
+//  'Misha',
+//  'Andrey',
+//  'Sasha',
+//  'Oleg',
+//  'Danil',
+//  'Dima',
+//  'Artur',
+//  'Ivan',
+//  'Slava',
+//  'Alisher',
+//  'Amiran',
+// ]
+// for (let i of allNamePersonal) {
+//   if (i == 'Ivan' || i == 'Oleg') {
+//     console.log(`${i}, вы не приглашены`);
+//   } else {
+//     console.log(`${i} вы приглаешны`);
+//   }
+// }
+
+// let arr =['Hello', 'World', 'My', 'name', 'is', 'Danil']
+// for (let i of arr){
+//   console.log(i)
+// }
+
+  //  const objPersonal = [
+  //   {
+  //     name: 'Misha',
+  //     age: 24,
+  //     position: 'Frontend Developer',
+  //     budget: '2300$',
+  //   },
+  //   {
+  //     name: 'Andrey',
+  //     age: 27,
+  //     position: 'Data Analizi',
+  //     budget: '2000$',
+  //   },
+  //   {
+  //     name: 'Sasha',
+  //     age: 32,
+  //     position: 'QA',
+  //     budget: '3200$',
+  //   },
+  //   {
+  //     name: 'Oleg',
+  //     age: 21,
+  //     position: 'Frontend Developer',
+  //     budget: '800$',
+  //   },
+  //   {
+  //     name: 'Danil',
+  //     age: 26,
+  //     position: 'Frontend Developer',
+  //     budget: '2000$',
+  //   },
+  //   {
+  //     name: 'Dima',
+  //     age: 29,
+  //     position: 'Backend Developer',
+  //     budget: '1700$',
+  //   },
+  //   {
+  //     name: 'Artur',
+  //     age: 24,
+  //     position: 'Testing',
+  //     budget: '500$',
+  //   },
+  //   {
+  //     name: 'Ivan',
+  //     age: 25,
+  //     position: 'C++ Developer',
+  //     budget: '1300$',
+  //   },
+  //   {
+  //     name: 'Slava',
+  //     age: 21,
+  //     position: 'Qa Testing',
+  //     budget: '1100$',
+  //   },
+  //   {
+  //     name: 'Alisher',
+  //     age: 22,
+  //     position: 'Web Designer',
+  //     budget: '1000$',
+  //   },
+  //   {
+  //     name: 'Amiran',
+  //     age: 26,
+  //     position: 'Graph Designer',
+  //     budget: '900$',
+  //   },
+  //  ]
+    
+
+  //  const products = [
+  //   {
+  //     title: 'Куртка Осеняя',
+  //     price: '2300$',
+  //   },
+  //   {
+  //     title: 'Шорты Puma',
+  //     price: '300$',
+  //   },
+  //   {
+  //     title: 'Кросовки Nike',
+  //     price: '1200$',
+  //   },
+  //   {
+  //     title: 'Очки Prada',
+  //     price: '700$',
+  //   },
+  //   {
+  //     title: 'Кофта',
+  //     price: '450$',
+  //   },
+  //  ]
+    
+
+  //  const productsNew = []
+  //  for(let i of products){
+  //   i.img = "хттп"
+  //   console.log(i)
+  //  }
+
+
+
+
